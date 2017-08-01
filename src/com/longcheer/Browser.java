265f4a0bf7786b7dcdef86f4de425e191c633282
ConcaSu <453813781@qq.com>
2017-08-01 16:38:04 +0800

    Slow down

--- src/com/longcheer/Browser.java
+++ src/com/longcheer/Browser.java
@@ -14,8 +14,15 @@ public class Browser extends UiAutomatorTestCase {
 	public void testDemo() throws UiObjectNotFoundException{
 		
 		device.pressHome();
+		try{Thread.sleep(3000);}catch(Exception e){}
+		device.click(700, 1750);
+		try{Thread.sleep(3000);}catch(Exception e){}
+		device.pressHome();
+		try{Thread.sleep(3000);}catch(Exception e){}
 		obj= new UiObject(new UiSelector().text("浏览器"));
+		try{Thread.sleep(3000);}catch(Exception e){}
 		device.click(500, 500);
+		try{Thread.sleep(3000);}catch(Exception e){}
 		device.pressHome();		
 	}
 
